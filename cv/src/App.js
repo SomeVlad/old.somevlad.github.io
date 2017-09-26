6ae831e761172cda592926affdcfdd11ec913184
Vlad Samoylov <samoylov.dev@gmail.com>
2017-09-27 01:15:58 +0300

    [WIP] cv page

--- cv/src/App.js
+++ cv/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import LanguageSwitcher from 'LanguageSwitcher'
+import LanguageSwitcher from './LanguageSwitcher'
 import './App.css'
 
 class App extends Component {
