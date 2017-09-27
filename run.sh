41a25f08cc09dabd811c5a8f65b8e74a0280915c
Vlad Samoylov <samoylov.dev@gmail.com>
2017-09-28 00:22:31 +0300

    exp

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ echo "Starting local server..."
 jekyll serve --watch --drafts &
 sleep 5
 #ngrok http 4000 
-osascript -e 'tell application "System Events" to keystroke "t" using command down' -e 'delay 1' -e 'tell application "System Events" to keystroke "ngrok http 4000"' -e 'tell application "System Events" to keystroke return'
+#osascript -e 'tell application "System Events" to keystroke "t" using command down' -e 'delay 1' -e 'tell application "System Events" to keystroke "ngrok http 4000"' -e 'tell application "System Events" to keystroke return'
